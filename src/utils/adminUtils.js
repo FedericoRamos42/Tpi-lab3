9a870915a80faa57fb366141e1883febb967dd5e
Federico Ramos <federicojramos42@gmail.com>
2024-11-08 11:27:44 -0300

    admin utils

---
+++ src/utils/adminUtils.js
@@ -0,0 +1,33 @@
+export const fetchFilteredUsers = async (query)=>{
+
+    // const query = new URLSearchParams();
+    // if (stateFiltered) query.append('state', stateFiltered);
+
+    const response = await fetch(`http://localhost:5190/api/User/Filtered?${query.toString()}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const users = response.json()
+    return users
+
+}
+
+export const fetchDeleteUser = async (id)=>{
+    const response = await fetch(`http://localhost:5190/api/User/Delete/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (!response.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+}
